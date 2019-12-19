8d5b4455d5bedfb9d3521babbf4e6ba92bb74080
harit1234 <hrtsaxena8@gmail.com>
2019-12-19 15:43:08 +0530

    corrected links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
   openMenuClass = false;
-  constructor(private router: Router) { }
-  public currentUrl = '';
+  constructor(private router: Router) {}
+  public currentUrl = "";
   ngOnInit() {
     this.currentUrl = this.router.url; /// this will give you current url
+  }
+  navigateToBfx() {
+    this.router.navigateByUrl("bfx");
   }
 }
