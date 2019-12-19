2748287f6e41cb5cb6b5348480cbf0789485f49b
Anil Kumar <anil.kumar@stigasoft.com>
2019-12-19 18:33:03 +0530

    Updated api ans web socket urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   name: 'prod',
   restUrl: 'http://52.211.112.45/api/v1/',
-  wssOMSURL: 'wss://staging-ws-order-v1.bitfex.com/',
+  wssOMSURL: 'ws://52.212.107.107:36822/',
   recaptchaKey: '6LdrWoIUAAAAAPHOs50VyHqS-RChEwgaSGlPI987',
   ipinfoAccessToken: "a4358b19f1bfdd",
   restrictedCountryCode: "US"
