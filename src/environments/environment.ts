cf8edc1dbe1334d075ed4417184f94df8c3b2efe
Anil Kumar <anil.kumar@stigasoft.com>
2020-01-10 19:27:50 +0530

    Updated backend server url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   name: 'dev',
-  restUrl: 'http://52.211.112.45/api/v1/',
+  restUrl: 'http://63.32.109.29/api/v1/',
   wssOMSURL: 'ws://52.212.107.107:36822/',
   recaptchaKey: '6LdrWoIUAAAAAPHOs50VyHqS-RChEwgaSGlPI987',
   ipinfoAccessToken: "a4358b19f1bfdd",
