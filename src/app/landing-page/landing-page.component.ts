60ee62bf2658ab443019d5e457e6322c433722c5
harit123 <harit.saxena@bitfex.com>
2020-03-28 15:04:50 +0530

    user

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -10,9 +10,39 @@ import { Router } from "@angular/router";
 export class LandingPageComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
+  launchDate: Date = new Date("4 Jul 2020");
+
+  timerString: string;
+
   ngOnInit() {
     if (this.authService.isAuthenticated()) {
       this.router.navigate(["dashboard"]);
     }
+    console.log(this.launchDate);
+    setInterval(() => {
+      this.timerString = this.getRunningTimer();
+    }, 1000);
+  }
+
+  getRunningTimer() {
+    let deadLine: Date = this.launchDate;
+    var today = new Date();
+    var hours = today.getUTCHours();
+
+    let distance = deadLine.getTime() - today.getTime();
+
+    let daysToShow = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    let hoursToShow = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return (
+      daysToShow + "D:" + hoursToShow + "H:" + minutes + "M:" + seconds + "S"
+    );
   }
 }
